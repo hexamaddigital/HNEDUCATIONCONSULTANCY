7703675dd55d6d6ccf5343e8d4b4d627feaab044
hexamaddigital <hexamad.digital@gmail.com>
2026-01-28 15:25:54 +0530

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { Mail, Phone, MapPin, Facebook, Instagram, Linkedin, Twitter } from 'lucide-react';
+import { Mail, Phone, MapPin, Facebook, Instagram, Linkedin, Twitter,Youtube } from 'lucide-react';
 import { COUNTRY_NAMES } from '../constants/countries';
 
 export const Footer = () => {
