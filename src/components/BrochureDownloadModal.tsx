e552e07fb855fbbbb28dafc6eb1c0aa9464f4f73
hexamaddigital <hexamad.digital@gmail.com>
2026-02-06 16:09:46 +0530

    Updated BrochureDownloadModal.tsx

--- src/components/BrochureDownloadModal.tsx
+++ src/components/BrochureDownloadModal.tsx
@@ -27,7 +27,7 @@ const UNIVERSITY_LIST_LINKS: Record<string, string> = {
   Netherlands: 'https://drive.google.com/uc?export=download&id=1cSrxH7VtxRiztHdzJEl5_35CpjEDwVgH',
   Switzerland: 'https://drive.google.com/uc?export=download&id=1uvO5m26usMCbr-TuuPs277xKxVo-fZwo',
   Sweden: 'https://drive.google.com/uc?export=download&id=10pcw-fnDxsifswrQ5vaNPIw8IiXEbEk4',
-  'New Zealand': 'https://drive.google.com/uc?export=download&id=1iXog-K3WqYRkkBTMeNoaqtHL11GuEpL7',
+  New Zealand: 'https://drive.google.com/uc?export=download&id=1iXog-K3WqYRkkBTMeNoaqtHL11GuEpL7',
   Dubai: 'https://drive.google.com/uc?export=download&id=1D5YTqyPKvbU79KZPabnCduumyq3bdgsP',
   Ireland: 'https://drive.google.com/uc?export=download&id=1eFvPqPVYDxICZ9pdMa1Y6B2MFjC6wucP',
   Spain: 'https://drive.google.com/uc?export=download&id=1HQTrVBWfgjOg5k6uc9XbtrzBMqp3PayT',
